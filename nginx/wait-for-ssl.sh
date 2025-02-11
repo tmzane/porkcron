b6ac7961ce85f302ee0f01d5df57cbab9e803814
Nik <73077675+tmzane@users.noreply.github.com>
2025-02-11 23:10:26 +0200

    feat: support multiple domains

--- nginx/wait-for-ssl.sh
+++ nginx/wait-for-ssl.sh
@@ -2,6 +2,7 @@
 
 ME=$(basename $0)
 
+# TODO: support multiple domains
 while [ ! -e /etc/porkcron/certificate.pem ] || [ ! -e /etc/porkcron/private_key.pem ]; do
 	echo "$ME: waiting for SSL bundle"
 	sleep 1
